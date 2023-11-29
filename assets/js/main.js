4639705300b206e26c5d8630ded0cd5a5cf68d9f
mastmagan007 <tshanajk@gmail.com>
2023-11-29 14:58:43 +0500

    Taimoor Updated Design

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,18 +78,17 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Scroll top button
    */
-  const scrollTop = document.querySelector('.scroll-top');
-  if (scrollTop) {
-    const togglescrollTop = function() {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  document.addEventListener('scroll', () => {
+    const header = document.querySelector('#header');
+    const videoSec = document.querySelector('#videoSec');
+
+    if (window.scrollY > 500) {
+        header.classList.add('headerScroll');
+    } else {
+        header.classList.remove('headerScroll');
     }
-    window.addEventListener('load', togglescrollTop);
-    document.addEventListener('scroll', togglescrollTop);
-    scrollTop.addEventListener('click', window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    }));
-  }
+});
+
 
   /**
    * Initiate glightbox
